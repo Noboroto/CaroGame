42c37dcbbf220363567f2b1373cefd1d69340d63
David0403 <48942146+Noboroto@users.noreply.github.com>
2021-11-28 09:20:35 +0700

    Update CaroGame.cpp
    - Print Cell
    - Print Board

--- CaroGame.cpp
+++ CaroGame.cpp
@@ -12,31 +12,31 @@ using std::ifstream;
 using std::ofstream;
 
 //LIMITATION
-const int MAX_PLAYER_PER_MAP = 4;
-const int MAX_HORSE_PER_PLAYER = 4;
-const int MAX_ROW = 10;
-const int MAX_COL = 10;
+const int MAX_ROW = 12;
+const int MAX_COL = 12;
 const int SPACE_BETWEEN_POINT = 2;
-const int MAX_NAME = 7;
-const int MAX_CHAR_DISPLAY = 9;
-const int MID_NAME_DISPLAY = MAX_CHAR_DISPLAY / 2;
+const int NAME_DISPLAY = 7;
+const int COL_SIZE = 9;
+const int ROW_SIZE = 5;
 const int MAX_ACCOUNT = 100;
 
 
 int winnerID = -1;
 int UserInputInt_ = 0;
 HANDLE ScreenHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+COORD CursorPosition;
+
 
 struct Player
 {
-	char Username[MAX_NAME + 1];
+	char Username[NAME_DISPLAY + 1];
 	int Win;
 	int Tie;
 	int Lose;
 
 	Player(int win = 0, int tie = 0, int Lose = 0)
 	{
-		memset(Username, '\0', MAX_NAME + 1);
+		memset(Username, '\0', NAME_DISPLAY + 1);
 		Win = win;
 		Tie = tie;
 	}
@@ -77,6 +77,13 @@ void saveAccounts()
 	file.close();
 }
 
+void moveCursor(int row, int col)
+{
+	CursorPosition.X = col;
+	CursorPosition.Y = row;
+	SetConsoleCursorPosition(ScreenHandle, CursorPosition);
+}
+
 struct Point
 {
 	int Col;
@@ -85,6 +92,7 @@ struct Point
 	int HorizontalLink = 0;
 	int MainCross = 0;
 	int ExtraCross = 0;
+	int CurrentPlayer = -1;
 	Point(int col = 0, int row = 0)
 	{
 		Col = col;
@@ -94,8 +102,7 @@ struct Point
 	void drawHorizontalLine()
 	{
 		cout << ' ';
-		const int DISPLAY_SIZE = MAX_CHAR_DISPLAY;
-		for (int i = 0; i < DISPLAY_SIZE; ++i)
+		for (int i = 0; i < COL_SIZE; ++i)
 		{
 			cout << '-';
 		}
@@ -104,7 +111,7 @@ struct Point
 	void drawBlankVerticalLin()
 	{
 		cout << '|';
-		for (int i = 0; i < MAX_CHAR_DISPLAY; ++i)
+		for (int i = 0; i < COL_SIZE; ++i)
 		{
 			cout << ' ';
 		}
@@ -112,28 +119,89 @@ struct Point
 	}
 	void drawPoint()
 	{
-		COORD Position;
-		Position.X = Col;
-		Position.Y = Row;
-		SetConsoleCursorPosition(ScreenHandle, Position);
+		moveCursor(Row, Col);
 		drawHorizontalLine();
-		Position.Y++;
-		SetConsoleCursorPosition(ScreenHandle, Position);
+		moveCursor(Row + 1, Col);
 		drawBlankVerticalLin();
-		Position.Y++;
-		SetConsoleCursorPosition(ScreenHandle, Position);
+		moveCursor(Row + 2, Col);
 		drawBlankVerticalLin();
-		Position.Y++;
-		SetConsoleCursorPosition(ScreenHandle, Position);
+		moveCursor(Row + 3, Col);
 		drawBlankVerticalLin();
-		Position.Y++;
-		SetConsoleCursorPosition(ScreenHandle, Position);
+		moveCursor(Row + 4, Col);
 		drawHorizontalLine();
+	}
+};
+
+struct Map
+{
+	Point Grid[MAX_ROW][MAX_COL];
+	char Player[2][NAME_DISPLAY];
+	int RowSize = 3;
+	int ColSize = 3;
+	int WiningMove = 3;
+	int TieCounter;
+
+	Map(int rowSize = 3, int colsize = 3)
+	{
+		RowSize = rowSize;
+		ColSize = colsize;
+		TieCounter = RowSize * colsize;
+		int y = 0;
+		for (int i = 1; i <= RowSize; ++i)
+		{
+			Grid[i][1] = Point(0, y);
+			for (int j = 2; j <= ColSize; ++j)
+			{
+				Grid[i][j] = Point(Grid[i][j - 1].Col + COL_SIZE + 2, y);
+			}
+			y += 5;
+		}
+	}
+
+	bool isWinner(int PlayerOrder)
+	{
+
+	}
+
+	void PrintMap()
+	{
+		SMALL_RECT WindowSize;
+		WindowSize.Top = 0;
+		WindowSize.Left = 0;
+		WindowSize.Right = ColSize * (COL_SIZE + 2);
+		WindowSize.Bottom = RowSize * ROW_SIZE + 2; 
+
+		COORD NewSize;
+		NewSize.X = WindowSize.Right;
+		NewSize.Y = WindowSize.Bottom;
+
+		SetConsoleWindowInfo(ScreenHandle, 1, &WindowSize);
+		SetConsoleScreenBufferSize(ScreenHandle, NewSize);
+		auto hWnd = GetConsoleWindow();
+		HMENU hMenu = GetSystemMenu(hWnd, false);
+
+		SetWindowLong(hWnd, GWL_STYLE, GetWindowLong(hWnd, GWL_STYLE) & ~WS_SIZEBOX);
+		DeleteMenu(hMenu, SC_MAXIMIZE, MF_BYCOMMAND);
+
+		for (int i = 1; i <= RowSize; ++i)
+		{
+			for (int j = 1; j <= ColSize; ++j)
+			{
+				Grid[i][j].drawPoint();
+			}
+		}
 	}
 };
 
 
 int main()
 {
+	Map test = Map(3, 7);
+	test.PrintMap();
+	char x;
+	while (true)
+	{
+		cin >> x;
+	}
 	return 0;
 }
